a13b794041cdc91cd0575126a716afd6583a95f2
Hisham Ibn Mahbub <hishamxs420@gmail.com>
2025-03-08 16:41:49 +0600

    committed mann

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+import Blogs from "./Components/Blogs/Blogs";
+import Header from "./Components/header/header";
+import { useEffect } from "react";
+
+function App() {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch('blogs.json')
+    .then(res => res.json())
+    .then(data => setBlogs(data));
+  },[])
+
+  return (
+    <>
+    <Header></Header>
+    <Blogs></Blogs>
+    </>
+  );
+}
+
+export default App;
